3f87d03764ff8e848de9795a8613baa14ed5e852
Terry-Diana <terry.wdiana@gmail.com>
2023-02-03 17:22:24 +0300

    sidebar update

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css";
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+      <h2>Dashboard</h2>
+      <h2>apprenticeship</h2>
+      <h2>Internships</h2>
+      <h2>Jobs</h2>
+      <h2>Settings</h2>
+    </div>
+  );
+}
+
+export default Sidebar;
