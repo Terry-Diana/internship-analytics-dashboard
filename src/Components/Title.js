1737271dccd99f416011cfe28a65a6fff3ddaf19
Terry-Diana <terry.wdiana@gmail.com>
2023-02-06 22:11:05 +0300

    Title section

---
+++ src/Components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div>
+      <h1 className="Title">Internships</h1>
+      <button className="Internshipbtn">Create New Internships</button>
+    </div>
+  );
+}
+
+export default Title;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Sidebar from "./Components/Sidebar";
+import Title from "./Components/Title";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Title />
     <Sidebar />
   </React.StrictMode>
 );
